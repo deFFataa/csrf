6c98e7b2d650c0f8ddeb7c71a333fdb5cc179771
NRCP <isaacbalabbo0819@gmail.com>
2025-06-26 16:03:55 +0800

    replaced select value to region.code

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 axios
   .get("https://psgc.gitlab.io/api/regions.json")
   .then((response) => {
+    console.log(response);
+    
     const regions = response.data;
     const regionSelect = document.getElementById("region");
     regions.forEach((region) => {
       const option = document.createElement("option");
-      option.value = `${region.regionName} (${region.name})`;
+      option.value = region.code;
       option.textContent = `${region.regionName} (${region.name})`;
       regionSelect.appendChild(option);
     });
   })
   .catch((error) => console.error("Error fetching regions:", error));
+  
